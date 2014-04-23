4453537bbbd31970af26251fca15d5b608f119a0
Roman Konstantinovich <krom.xr@gmail.com>
2014-04-24 05:02:35 +0600

    checking with interval

--- feed.js
+++ feed.js
@@ -1,27 +1,66 @@
+/*global BASE_URL*/
+var feed = function(dom_el_id, user_id, fn, options) {
 
+    var feed_ob = {
+        generateCallbackName: function() {
+            return 'callb_' + new Date().getTime();
+        },
+        getFeed: function(url, user_id, callback) {
+            var data;
+            var callback_name = this.generateCallbackName();
 
+            window[callback_name] = function(_data) { data = _data; };
 
-var feed = {
-    generateCallbackName: function() {
-        return 'callb_' + new Date().getTime();
-    },
-    getFeed: function(user_id, callback) {
-        var data;
-        var callback_name = this.generateCallbackName();
+            var script = document.createElement('script');
+            if (!this.head) {
+                 this.head = document.getElementsByTagName('head')[0];
+            }
+            this.head.appendChild(script);
 
-        window[callback_name] = function(_data) { data = _data; };
+            script.onload = function() {
+                callback(data);
+                script.remove();
+                delete window[callback_name];
+            };
+            script.onerror = function(e) { console.error(e); };
+            script.src = BASE_URL + url + '?callback=' + callback_name + '&user_id=' + user_id;
+        },
+        startChecking: function(timeout) {
+            var it = this;
+            it.getFeed(it.url, it.user_id, it.render);
+            it.interval = setInterval(function() {
+                it.getFeed(it.url, it.user_id, it.render);
+            }, it.check_timeout);
+        },
+        stopChecking: function() {
+            clearInterval(this.interval);
+        },
+        render: function(data) {
+            console.log(data);
+        }
+    };
 
-        var script = document.createElement('script');
-        var head = document.getElementsByTagName('head')[0];
-        head.appendChild(script);
+    options = options || {};    
 
-        script.onload = function() { callback(data); };
-        script.src = 'http://localhost:8000/js-api/unread-feed-item/?callback=' + callback_name + '&user_id=' + user_id;
-    }
+    feed_ob.check_timeout = options.check_timeout || 5000;
+    feed_ob.url = options.url || "/js-api/unread-feed-item/";
 
+    feed_ob.startChecking();
+    return feed_ob;
+}
 
-};
+feed('feed', 534, function() {
 
-feed.getFeed(15, function(data) {
-    console.log(data);
+}, {
+    check_timeout: 1000,
+    url: "/js-api/unread-feed-item/",
 });
+
+//feed().setSettings({
+    //user_id: 534,
+    //check_timeout: 1000
+//}).startChecking();
+
+//setTimeout(function() {
+    //feed.stopChecking();
+//}, 10000);
